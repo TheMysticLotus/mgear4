953a2a826126d8c20d4688c6e14b87ae4efbc8eb
Ehsan Kiani <ehsankiani.tech@gmail.com>
2021-08-11 18:38:27 -0700

    Added shifter_custom_components path to mgear.

--- release/scripts/mgear/shifter/__init__.py
+++ release/scripts/mgear/shifter/__init__.py
@@ -18,6 +18,9 @@ from . import guide, component
 from mgear.core import primitive, attribute, skin, dag, icon, node
 from mgear import shifter_classic_components
 from mgear import shifter_epic_components
+
+from ThirdParty.mgear4_customized.shifter_custom_components
+
 from mgear.shifter import naming
 import importlib
 
@@ -66,7 +69,8 @@ def getComponentDirectories():
     return mgear.core.utils.gatherCustomModuleDirectories(
         SHIFTER_COMPONENT_ENV_KEY,
         [os.path.join(os.path.dirname(shifter_classic_components.__file__)),
-         os.path.join(os.path.dirname(shifter_epic_components.__file__))])
+         os.path.join(os.path.dirname(shifter_epic_components.__file__)),
+         os.path.join(os.path.dirname(shifter_custom_components.__file__))])
     # return mgear.core.utils.gatherCustomModuleDirectories(
     #     SHIFTER_COMPONENT_ENV_KEY,
     #     os.path.join(os.path.dirname(shifter_classic_components.__file__)))
